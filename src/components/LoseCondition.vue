52320b40a26b267cdb662485fd9c7832da13a377
edwinsatya <edwinsatyayudistira@gmail.com>
2019-12-13 15:08:46 +0700

    update condition win,lose

---
+++ src/components/LoseCondition.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="lose-condition">
+    <div v-if="$route.path == '/lose'" class="fullscreen-video-wrap">
+      <video
+        id="vidio-bg"
+        src="../assets/video/losecondition.mp4"
+        autoplay="true"
+      ></video>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoseCondition",
+  methods: {
+    goToClosing() {
+      setTimeout(() => {
+        this.$router.push("/close");
+      }, 8000);
+    }
+  },
+  created() {
+    this.goToClosing();
+  }
+};
+</script>
+
+<style scoped>
+.fullscreen-video-wrap {
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.fullscreen-video-wrap video {
+  min-height: 100%;
+  min-width: 100%;
+}
+</style>
---
+++ src/components/WinCondition.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="win-condition">
+    <div v-if="$route.path == '/win'" class="fullscreen-video-wrap">
+      <video
+        id="vidio-bg"
+        src="../assets/video/wincondition.mp4"
+        autoplay="true"
+      ></video>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WinCondition",
+  methods: {
+    goToClosing() {
+      setTimeout(() => {
+        this.$router.push("/close");
+      }, 6000);
+    }
+  },
+  created() {
+    this.goToClosing();
+  }
+};
+</script>
+
+<style scoped>
+.fullscreen-video-wrap {
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.fullscreen-video-wrap video {
+  min-height: 100%;
+  min-width: 100%;
+}
+</style>
